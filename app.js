aa6fc8decc9d267a1c75132a06bad09fd86103a9
srestrepoo <srestrepoo@unal.edu.co>
2019-03-03 18:49:36 -0500

    port change

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/api", routes);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
 
